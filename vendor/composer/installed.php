0a8f098527e7dfa3556872ef6e4dafa0db0a8a6a
Duda-Santosr <maria.e.santosr13@gmail.com>
2025-04-28 11:24:26 -0300

    commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'locadora/veiculos',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '18fa15ea451dc9ccf6a4d59e4805281365f344e1',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'locadora/veiculos' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '18fa15ea451dc9ccf6a4d59e4805281365f344e1',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
